219af1f240c8be95fb1d29511c0c4841292a7ff4
juliashalneva <yuliashalneva@yandex.ru>
2018-10-01 14:19:24 +0500

    изменение

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   
+                int n = 1000;
+                 int sum = 0;
+                 for (int i = 0; i < n; i++)
+                 {
+
+                     if (i % 3 == 0 ||  i % 5 ==0)
+
+                            sum = sum + i;
+
+                 }
+                 Console.WriteLine(sum);
+
+                 Console.ReadLine();
+             
+        }
+    }
+}
